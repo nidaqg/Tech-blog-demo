4fd4754859d7162e9bdf970538f518b82127e775
nqg <nidaghuman@gmail.com>
2021-03-25 11:45:05 -0400

    basic code in config and server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+//require all dependencies
+const path = require('path');
+const express = require('express');
+
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+//initialize express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions
+const sess = {
+  secret: 'Super Top Secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at ${PORT}!`));
+});
